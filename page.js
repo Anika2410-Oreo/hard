6d7cf80a6baed13a5f968ce06143eac460351610
Anika2410-Oreo <khandekar.anika24@gmail.com>
2024-10-19 09:34:21 +0530

    Add files via upload

--- page.js
+++ page.js
@@ -1,16 +1,18 @@
+import DemoSection from "@/components/DemoSection";
 import PageHeaders from "@/components/PageHeaders";
+import UploadForm from "@/components/UploadForm";
 
-export default function PricingPage() {
+export default function Home() {
   return (
-    <div>
+    <>
       <PageHeaders
-        h1Text={'Check out our pricing'}
-        h2Text={'Our pricing is very simple'} />
-
-      <div className="bg-white text-slate-700 rounded-lg max-w-xs mx-auto p-4 text-center">
-        <h3 className="font-bold text-3xl">Free</h3>
-        <h4>Free forever</h4>
+        h1Text={'Add epic captions to your videos'}
+        h2Text={'Just upload your video and we will do the rest'}
+      />
+      <div className="text-center">
+        <UploadForm />
       </div>
-    </div>
-  );
+      <DemoSection />
+    </>
+  )
 }
